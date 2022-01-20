5a11fb179052cef5aab9c38213406737090ff55b
Avinash Dwivedy <avinash.dwivedy@yahoo.in>
2022-01-20 12:52:32 +0530

    committed

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Avinash");
-		System.out.println("Hi Avinash.you are working eccellent ok bravo yescarry on");
+		System.out.println("Hi Avinash.you are working eccellent ok avinash bravo yescarry on");
 
 }
 }
